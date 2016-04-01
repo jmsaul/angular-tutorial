4cf8089c8152bc60c7c5db302f5538219d727367
Jeff Saul <scaleup13@gmail.com>
2016-03-31 22:33:59 -0600

    take 1 for angular form put into contact page + form controllers in script.js

--- public_html/onePageApp-take1/script.js
+++ public_html/onePageApp-take1/script.js
@@ -42,3 +42,64 @@ scotchApp.controller('contactController', function($scope) {
 	$scope.message = 'Contact us!  JK.  This is just a demo.';
 });
 
+scotch.controller('AngularFormController", ["$scope",function($scope) {
+	/**
+	 * state variable to store the alerts gnerated from the submit event
+	 * @type {Array}
+	 **/
+	$scope.alerts = [];
+
+	/**
+	 * state variable that gnerates the checkboxes and keeps track of which boxes are checked
+	 * @type {object}
+	 */
+	$scope.cats = {"Arlo": false, "Garfield": false, "Grumpy Cat": false, "Hello Kitty": false};
+
+	/**
+	 * state variable to keep track of the data entered into the form fields
+	 * @type {object}
+	 */
+	$scope.formData = {"bid": 1.0, "favoriteKitties": [], "fullName": null};
+
+	/**
+	 * method to rewrite the cats array when a checkbox is clicked
+	 */
+	$scope.checkKitty = function() {
+		$scope.formData.favoriteKitties [];
+		for(cat is $scope.cats) {
+				if($scope.cats[cat] === true) {
+					$scope.formData.favoriteKitties.push(cat);
+				}
+		}
+	};
+
+	/**
+	 * method to reset form data when the submit and cancel buttons are pressed
+	 */
+	$scope.reset = function() {
+		$scope.formData = {bid: 1.0, favoriteKitties: [], fullName: null};
+		$scope.sampleForm.$setUntouched();
+		$scope.sampleForm.$setPristine();
+	};
+
+	/**
+	 * method to process the action from the submit button
+	 *
+	 * @param formData object containing submitted form data
+	 * @param validated true if passed validation, false if not
+	 */
+	$scope.submit = function(formData, validated) {
+		if(validated === true) {
+			$scope.alerts[0] = {
+				type: "success",
+				msg: "Well done! You have found the submit on this form and clicked it"
+			};
+		} else {
+			$scope.alerts[0] = {
+				type: "danger",
+				msg: "Oh snap! You clicked the submit button while the form has invalid data.  Check your self before you wreck yourself!"
+			};
+		}
+		$scope.reset();
+	};
+}]);
